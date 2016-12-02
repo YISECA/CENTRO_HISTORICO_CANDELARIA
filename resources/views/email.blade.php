7769c9818241e8bd3c483147d23ee9038b0efb1d
YISECA <yiseca@gmail.com>
2016-12-02 12:08:19 -0500

    Formulario Centro Histórico Candelaria

---
+++ resources/views/email.blade.php
@@ -0,0 +1,13 @@
+<html>
+
+
+
+<h1>CAMINATA CENTRO HISTÓRICO LA CANDELARIA 04  de Diciembre de 2016</h1>
+
+
+
+<h1>Su registro fue exitoso!</h1>
+
+
+
+<p></p>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,274 @@
+@extends('master')                              
+
+@section('content')         
+
+<link rel="stylesheet" type="text/css" href="public/Css/form.css">
+
+<link rel="stylesheet" type="text/css" href="public/Css/slider_styles.css">
+
+<section id="page1">
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">Inicio</div>
+
+        <div class="panel-body">
+
+            <div class="freebirdFormviewerViewFormContent ">
+
+                <div class="freebirdFormviewerViewHeaderHeader">
+
+                    <div class="freebirdFormviewerViewHeaderTitleRow">
+
+                        <div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1">CENTRO HISTÓRICO
+LA CANDELARIA 04 de Diciembre de 2016</div>
+
+                    </div>
+<br>
+                    <div class="stage">
+
+                        <div id="SLDR-ONE" class="sldr">
+
+                            <ul class="wrp animate">
+
+                                <li class="elmnt-one">
+
+                                    
+
+                                        <div class="wrap"><img src="public/Img/candelaria.jpg" width="800" height="563">
+
+                                        </div>
+
+                                    
+
+                                </li>
+
+                                <li class="elmnt-two">
+
+                                    
+
+                                        <div class="wrap"><img src="public/Img/candelaria.jpg" width="800" height="563">
+
+                                        </div>
+
+                                  
+
+                                </li>
+
+                            </ul>
+
+                        </div>
+
+                        <div class="clear"></div>
+
+                        <ul class="selectors">
+
+                            <li class="focalPoint"><a href="">•</a></li>
+
+                            <li><a href="">•</a></li>
+                         
+                        </ul>
+
+                    </div>
+
+                    <script src="public/Js/jquery.sldr.js"></script>
+
+                    <div class="freebirdFormviewerViewHeaderDescription" dir="auto">
+
+                        <h2>Ubicación</h2>
+
+                        <p><font size="3">El centro Histórico La Candelaria esta ubicado en el centro oriente de Bogotá,  localidad 17. Allí tienen lugar los acontecimientos históricos, políticos y sociales determinantes para nuestra ciudad y país; Desde la fundación de la ciudad más importante en El Reino de la Nueva Granada, hasta las manifestaciones socio-políticas actuales. En el recorrido se puede observar diferentes estilos arquitectónicos, Iglesias de siglo XVII, calles empedradas y conocer una gran oferta cultural y gastronómica. </font></p>
+
+                        <h2>Descripción del recorrido </h2>
+
+                        <p><font size="3">EEl recorrido da inicio en la plaza de Bolívar, donde se realizara un calentamiento de aproximadamente 20 minutos, el guía de caminatas del IDRD dará las recomendaciones así como las prohibiciones y restricciones propias del recorrido y necesarias para su ejecución y se dará inicio al recorrido.</font></p>
+
+                        <h2>Durante el recorrido se visitaran los siguientes atractivos:</h2>
+                        <ul class="ul_inicio">
+                            <font size="3">
+                            <li>Plaza de Bolívar y su entorno</li>
+                            <li>Calle 10 hasta la cra 4</li>
+                            <li>Manzana Cultural</li>
+                            <li>Chorro de Quevedo</li>
+                            <li>Eje ambiental</li>
+                            <li>Parque Santander</li>
+                            </font>
+                        </ul>
+                        <br>
+
+                        <font size="4"><center><table width="1000">
+
+                            <col width="281">
+
+                                <col width="282">
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Punto de encuentro</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Costado sur-oriental de la Plaza de Bolívar. Colegio San Bartolomé</p>
+
+                                        </td>
+
+                                    </tr>
+                              
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Fecha</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>04 de Diciembre de 2016</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Hora</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>8:00 a.m</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Cupo Máximo</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>40 Personsa</p>
+
+                                        </td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Rango de Edad</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #5b9bd5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>“APTA  PARA TODA LA FAMILIA”</p>
+
+                                        </td>
+
+                                    </tr>
+
+
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Altitud</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#deeaf6" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>2.600 m.s.n.m</td>
+
+                                    </tr>
+
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Temperatura</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>14°</p>
+
+                                        </td>
+
+                                    </tr>
+                                    <tr valign="top">
+
+                                        <td width="281" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>Duración</p>
+
+                                        </td>
+
+                                        <td width="282" bgcolor="#ffffff" style="border: 1px solid #9cc2e5; padding-top: 0cm; padding-bottom: 0cm; padding-left: 0.16cm; padding-right: 0.19cm">
+
+                                            <p>3     horas</p>
+
+                                        </td>
+
+                                    </tr>
+                                     
+
+                        </table></center></font>
+
+                        <h2>Recomendaciones</h2>
+
+                        <ul class="ul_inicio">
+
+                            <font size="3">
+
+                                <li>Llevar ropa cómoda</li>
+                            
+                                <li>Llevar bloqueador solar</li>
+
+                                <li>Hidratación</li>
+
+                                <li>Calzado cómodo</li>
+
+                                <li>Llevar carné de la eps</li>
+
+                                <li>Refrigerio.</li>
+
+                                <li>Medicamentos personales </li>
+
+                                <li>No llevar mascotas </li>
+
+                                <li>Abstenerse de participar: mujeres en estado de embarazo, personas que tengan problemas de salud complejos</li>
+
+                                <li><ins>Abstenerse de asistir si no se encuentra inscrito</ins></li>
+                            </font>
+
+                            <br>
+
+                        </ul> <a class="btn btn-info" href="registro">Registrarse</a> </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+@stop
